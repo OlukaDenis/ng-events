29c30b997a0d3b3cb1339455ada7029e33088411
OlukaDenis <olukadeno@gmail.com>
2023-01-09 18:17:59 +0300

    :hammer: Manage view visibility with ngSwitchCase

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,18 +6,23 @@ import { Component, Input } from "@angular/core";
          <div class="well hoverwell thumbnail">
             <h2>{{event?.name}}</h2>
             <div>Date: {{event?.date}}</div>
-            <div>Time: {{event?.time}}</div>
+            <div [ngSwitch]="event?.time">Time: {{event?.time}}
+                <span *ngSwitchCase="'8:00 am'"><i class="event-start">(Early Start)</i></span>
+                <span *ngSwitchCase="'10:00 am'"><i class="event-start">(Late Start)</i></span>
+                <span *ngSwitchDefault><i class="event-start">(Normal Start)</i></span>
+            </div>
             <div>Price: \${{event?.price}}</div>
-            <div [hidden]="!event?.location">
+            <div *ngIf="event?.location">
                 <span>Location: {{event?.location?.address}}</span>
                 <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
             </div>
-            <div [hidden]="!event?.onlineUrl">Online URL: {{event?.onlineUrl}}</div>
+            <div *ngIf="event?.onlineUrl">Online URL: {{event?.onlineUrl}}</div>
         </div>
     `,
     styles: [`
         .thumbnail { min-height: 210px;}
         .pad-left { margin-left: 8px}
+        .event-start { font-size: 12px }
     `]
 })
 export class EventThumbnailComponent {
